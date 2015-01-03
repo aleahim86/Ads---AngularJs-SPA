32f7cb6113f448f1d8c207a23692d8e9ca2e4caf
aleahim86 <alea_him@abv.bg>
2015-01-03 15:47:41 +0200

    Added routing - home, registerTemplate added

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,16 @@
 'use strict';
 
 var adsApp = angular
-        .module('adsApp', []);
-    
+        .module('adsApp', ['ngRoute'])
+        .config(function ($routeProvider) {
+            $routeProvider.when('/', {
+                templateUrl: 'templates/home.html',
+                controller: 'AdsAppController'
+            }).when('/register', {
+                templateUrl: 'templates/registerTemplate.html',
+                controller: 'Register'
+            })
+                    .otherwise('/'); 
+        });
+        
+
